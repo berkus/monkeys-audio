502310c684bae2754b4276160d357e86a624e9fd
Berkus Decker <berkus@madfire.net>
2018-03-03 00:11:55 +0200

    Use regular fprintf

--- Source/Console/Console.cpp
+++ Source/Console/Console.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************************
 MAC Console Frontend (MAC.exe)
 
-Pretty simple and straightforward console front end.  If somebody ever wants to add 
+Pretty simple and straightforward console front end.  If somebody ever wants to add
 more functionality like tagging, auto-verify, etc., that'd be excellent.
 
 Copyrighted (c) 2000 - 2018 Matthew T. Ashland.  All Rights Reserved.
@@ -26,25 +26,25 @@ TICK_COUNT_TYPE g_nInitialTickCount = 0;
 /***************************************************************************************
 Displays the proper usage for MAC.exe
 ***************************************************************************************/
-void DisplayProperUsage(FILE * pFile) 
+void DisplayProperUsage(FILE * pFile)
 {
-	_ftprintf(pFile, _T("Proper Usage: [EXE] [Input File] [Output File] [Mode]\n\n"));
+	fprintf(pFile, _T("Proper Usage: [EXE] [Input File] [Output File] [Mode]\n\n"));
 
-	_ftprintf(pFile, _T("Modes: \n"));
-	_ftprintf(pFile, _T("    Compress (fast): '-c1000'\n"));
-	_ftprintf(pFile, _T("    Compress (normal): '-c2000'\n"));
-	_ftprintf(pFile, _T("    Compress (high): '-c3000'\n"));
-	_ftprintf(pFile, _T("    Compress (extra high): '-c4000'\n"));
-	_ftprintf(pFile, _T("    Compress (insane): '-c5000'\n"));
-	_ftprintf(pFile, _T("    Decompress: '-d'\n"));
-	_ftprintf(pFile, _T("    Verify: '-v'\n"));
-	_ftprintf(pFile, _T("    Convert: '-nXXXX'\n\n"));
+	fprintf(pFile, _T("Modes: \n"));
+	fprintf(pFile, _T("    Compress (fast): '-c1000'\n"));
+	fprintf(pFile, _T("    Compress (normal): '-c2000'\n"));
+	fprintf(pFile, _T("    Compress (high): '-c3000'\n"));
+	fprintf(pFile, _T("    Compress (extra high): '-c4000'\n"));
+	fprintf(pFile, _T("    Compress (insane): '-c5000'\n"));
+	fprintf(pFile, _T("    Decompress: '-d'\n"));
+	fprintf(pFile, _T("    Verify: '-v'\n"));
+	fprintf(pFile, _T("    Convert: '-nXXXX'\n\n"));
 
-	_ftprintf(pFile, _T("Examples:\n"));
-	_ftprintf(pFile, _T("    Compress: mac.exe \"Metallica - One.wav\" \"Metallica - One.ape\" -c2000\n"));
-	_ftprintf(pFile, _T("    Decompress: mac.exe \"Metallica - One.ape\" \"Metallica - One.wav\" -d\n"));
-	_ftprintf(pFile, _T("    Verify: mac.exe \"Metallica - One.ape\" -v\n"));
-	_ftprintf(pFile, _T("    (note: int filenames must be put inside of quotations)\n"));
+	fprintf(pFile, _T("Examples:\n"));
+	fprintf(pFile, _T("    Compress: mac.exe \"Metallica - One.wav\" \"Metallica - One.ape\" -c2000\n"));
+	fprintf(pFile, _T("    Decompress: mac.exe \"Metallica - One.ape\" \"Metallica - One.wav\" -d\n"));
+	fprintf(pFile, _T("    Verify: mac.exe \"Metallica - One.ape\" -v\n"));
+	fprintf(pFile, _T("    (note: int filenames must be put inside of quotations)\n"));
 }
 
 /***************************************************************************************
@@ -62,7 +62,7 @@ void CALLBACK ProgressCallback(int nPercentageDone)
 	double dRemaining = dElapsed * ((1.0 / dProgress) - 1.0);							// seconds
 
 	// output the progress
-	_ftprintf(stderr, _T("Progress: %.1f%% (%.1f seconds remaining, %.1f seconds total)          \r"), 
+	fprintf(stderr, _T("Progress: %.1f%% (%.1f seconds remaining, %.1f seconds total)          \r"),
 		dProgress * 100, dRemaining, dElapsed);
 }
 
@@ -77,12 +77,12 @@ int _tmain(int argc, TCHAR * argv[])
 	int nMode = UNDEFINED_MODE;
 	int nCompressionLevel;
 	int nPercentageDone;
-		
+
 	// output the header
-	_ftprintf(stderr, CONSOLE_NAME);
-	
+	fprintf(stderr, CONSOLE_NAME);
+
 	// make sure there are at least four arguments (could be more for EAC compatibility)
-	if (argc < 3) 
+	if (argc < 3)
 	{
 		DisplayProperUsage(stderr);
 		exit(-1);
@@ -100,7 +100,7 @@ int _tmain(int argc, TCHAR * argv[])
 	// verify that the input file exists
 	if (!FileExists(spInputFilename))
 	{
-		_ftprintf(stderr, _T("Input File Not Found...\n\n"));
+		fprintf(stderr, _T("Input File Not Found...\n\n"));
 		exit(-1);
 	}
 
@@ -111,7 +111,7 @@ int _tmain(int argc, TCHAR * argv[])
 	if (_tcsnicmp(cMode, _T("-v"), 2) != 0)
 	{
 		// verify is the only mode that doesn't use at least the third argument
-		if (argc < 4) 
+		if (argc < 4)
 		{
 			DisplayProperUsage(stderr);
 			exit(-1);
@@ -133,19 +133,19 @@ int _tmain(int argc, TCHAR * argv[])
 		nMode = CONVERT_MODE;
 
 	// error check the mode
-	if (nMode == UNDEFINED_MODE) 
+	if (nMode == UNDEFINED_MODE)
 	{
 		DisplayProperUsage(stderr);
 		exit(-1);
 	}
 
 	// get and error check the compression level
-	if (nMode == COMPRESS_MODE || nMode == CONVERT_MODE) 
+	if (nMode == COMPRESS_MODE || nMode == CONVERT_MODE)
 	{
 		nCompressionLevel = _ttoi(&cMode[2]);
-		if (nCompressionLevel != 1000 && nCompressionLevel != 2000 && 
+		if (nCompressionLevel != 1000 && nCompressionLevel != 2000 &&
 			nCompressionLevel != 3000 && nCompressionLevel != 4000 &&
-			nCompressionLevel != 5000) 
+			nCompressionLevel != 5000)
 		{
 			DisplayProperUsage(stderr);
 			return -1;
@@ -154,10 +154,10 @@ int _tmain(int argc, TCHAR * argv[])
 
 	// set the initial tick count
 	TICK_COUNT_READ(g_nInitialTickCount);
-	
+
 	// process
 	int nKillFlag = 0;
-	if (nMode == COMPRESS_MODE) 
+	if (nMode == COMPRESS_MODE)
 	{
 		TCHAR cCompressionLevel[16];
 		if (nCompressionLevel == 1000) { _tcscpy(cCompressionLevel, _T("fast")); }
@@ -166,29 +166,29 @@ int _tmain(int argc, TCHAR * argv[])
 		if (nCompressionLevel == 4000) { _tcscpy(cCompressionLevel, _T("extra high")); }
 		if (nCompressionLevel == 5000) { _tcscpy(cCompressionLevel, _T("insane")); }
 
-		_ftprintf(stderr, _T("Compressing (%s)...\n"), cCompressionLevel);
+		fprintf(stderr, _T("Compressing (%s)...\n"), cCompressionLevel);
 		nRetVal = CompressFileW(spInputFilename, spOutputFilename, nCompressionLevel, &nPercentageDone, ProgressCallback, &nKillFlag);
 	}
-	else if (nMode == DECOMPRESS_MODE) 
+	else if (nMode == DECOMPRESS_MODE)
 	{
-		_ftprintf(stderr, _T("Decompressing...\n"));
+		fprintf(stderr, _T("Decompressing...\n"));
 		nRetVal = DecompressFileW(spInputFilename, spOutputFilename, &nPercentageDone, ProgressCallback, &nKillFlag);
-	}	
-	else if (nMode == VERIFY_MODE) 
+	}
+	else if (nMode == VERIFY_MODE)
 	{
-		_ftprintf(stderr, _T("Verifying...\n"));
+		fprintf(stderr, _T("Verifying...\n"));
 		nRetVal = VerifyFileW(spInputFilename, &nPercentageDone, ProgressCallback, &nKillFlag);
-	}	
-	else if (nMode == CONVERT_MODE) 
+	}
+	else if (nMode == CONVERT_MODE)
 	{
-		_ftprintf(stderr, _T("Converting...\n"));
+		fprintf(stderr, _T("Converting...\n"));
 		nRetVal = ConvertFileW(spInputFilename, spOutputFilename, nCompressionLevel, &nPercentageDone, ProgressCallback, &nKillFlag);
 	}
 
-	if (nRetVal == ERROR_SUCCESS) 
-		_ftprintf(stderr, _T("\nSuccess...\n"));
-	else 
-		_ftprintf(stderr, _T("\nError: %i\n"), nRetVal);
+	if (nRetVal == ERROR_SUCCESS)
+		fprintf(stderr, _T("\nSuccess...\n"));
+	else
+		fprintf(stderr, _T("\nError: %i\n"), nRetVal);
 
 	return nRetVal;
 }
