149c5648cbd0d5c5a339516c8aaf3fde5e852a56
Berkus Decker <berkus@madfire.net>
2018-03-02 23:21:43 +0200

    Add cmake config file
    From https://github.com/fernandotcl/monkeys-audio/

---
+++ Source/Shared/MACUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "config.h"
+
+#ifndef HAVE_WCSCASECMP
+#include <wchar.h>
+
+int mac_wcscasecmp(const wchar_t *s1, const wchar_t *s2);
+int mac_wcsncasecmp(const wchar_t *s1, const wchar_t *s2, size_t n);
+
+#endif
